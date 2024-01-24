34a3a461119bbaa0e2973cb713128cbb1bbcf6d7
root <root@MMENYENE-IDIONG>
2024-01-24 21:06:04 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts function
- * Rturn: 0 (Success)
+ * main - Entry point
+ * Description: A program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.
+ * Rturn: Always  0 (Success)
  */
 int main(void)
 {
