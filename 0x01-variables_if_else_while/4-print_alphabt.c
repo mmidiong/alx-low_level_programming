aa04bce971c1e89369f868eaae3900479124942c
root <root@MMENYENE-IDIONG>
2024-02-01 11:30:19 +0100

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,19 +7,18 @@
 int main(void)
 {
 	char c;
-	
+
 	c = 'a';
 
-	while 
+	while
 	(c <= 'z') {
-	if (c != 'q' &&  c != 'e') { 
+	if (c != 'q' &&  c != 'e')
+
 	putchar(c);
-	}
+
 	c++;
 	}
 
 	putchar('\n');
 	return (0);
 }
-	
-
