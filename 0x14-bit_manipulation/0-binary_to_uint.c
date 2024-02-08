a47eac67a195e9c8fbd7080dbab96a42bcb1798a
root <root@MMENYENE-IDIONG>
2024-02-08 22:06:09 +0100

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - A function that converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars.
+ *
+ * b is NULL.
+ *
+ * Return: Return the converted number or 0 if there is one or more chars in
+ * the string b that is not 0 or 1.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i, usigned_int = 0, dec_val = 1;
+
+	if (!b)
+		return (0);
+
+
+	for (i = _strlen(b) - 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		if (b[i] == '1')
+		{
+			usigned_int += dec_val;
+		}
+		dec_val *= 2;
+	}
+	return (usigned_int);
+}
+
+/**
+ * _strlen - Function that returns the length of string.
+ * @s: pointer to the null-terminated byte string to be examined.
+ *
+ * Return: The length of the null-terminated byte string s.
+ */
+
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
