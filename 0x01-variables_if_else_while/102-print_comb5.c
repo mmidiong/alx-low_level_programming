88992b0be1f7377ce1e5ad889616785b1d755ffe
root <root@MMENYENE-IDIONG>
2024-02-14 14:54:04 +0100

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all possible combination of two digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens, ones;
+
+	for (tens = 0; tens <= 99; tens++)
+	{
+		for (ones = 0; ones <= 99; ones++)
+		{
+			if (tens < ones)
+			{
+				putchar((tens / 10) + 48);
+				putchar((tens % 10) + 48);
+				putchar(' ');
+				putchar((ones / 10) + 48);
+				putchar((ones % 10) + 48);
+				if (tens != 98 && ones != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+
+	}
+	putchar('\n');
+
+	return (0);
+}
