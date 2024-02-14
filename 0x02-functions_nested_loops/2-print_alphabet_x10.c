6c2310afec6f254a9d3d58820b7528bba8058e95
root <root@MMENYENE-IDIONG>
2024-02-14 16:10:45 +0100

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,12 @@ void print_alphabet_x10(void)
 	int x;
 	char c;
 
-	for (x = 1; x <= 10, x++) {
-	for (c = 'a'; c <= 'z'; c++) {
-	_putchar(c);
-	_putchar('\n');
+	for (x = 1; x <= 10; x++)
+	{
+		for (c = 'a'; c <= 'z'; c++) 
+		{
+			_putchar(c);
 		}
+			_putchar('\n');
 	}
 }
