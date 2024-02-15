93d2b144b7256a3bd9ae088436843208dd932248
root <root@MMENYENE-IDIONG>
2024-02-15 08:51:19 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,11 +15,11 @@ int print_sign(int n)
 	printf("+");
 	return (1);
 
-	else if (n == 0)
-	printf("0");
-	return (0);
-
-	else if (n < 0)
+	else if (n > 0)
 	printf("-");
 	return (-1);
+
+	else
+	printf("0");
+	return (0);
 }
