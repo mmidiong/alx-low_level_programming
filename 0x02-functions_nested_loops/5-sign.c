53a6d9a78f74f4b730055232a34685da5e2733af
root <root@MMENYENE-IDIONG>
2024-02-15 08:59:24 +0100

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,21 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	printf("+");
-	return (1);
+	{
+		_putchar(43);
+		return (1);
+	}
 
 	else if (n > 0)
-	printf("-");
-	return (-1);
-
+	{
+		_putchar(45);
+		return (-1);
+	}
 	else
-	printf("0");
-	return (0);
+	{
+		_putchar(48);
+		return (0);
+	}
+	
+	_putchar('\n');
 }
