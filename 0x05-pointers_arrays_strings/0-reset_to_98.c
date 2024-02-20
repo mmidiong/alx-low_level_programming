e91eaaa825e90187af085cda86a31295517883a3
root <root@MMENYENE-IDIONG>
2024-02-20 12:12:02 +0100

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - A function that takes a pointer to an int as a 
+ * reset_to_98 - A function that takes a pointer to an int as a
  * parameter and updates value to 98.
  * @n: integer
  *
